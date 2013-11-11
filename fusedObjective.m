cbe395f2b7af54c887bd1aa08533e10c7e4826ef
unknown <Nicole@Computress.(none)>
2013-11-11 10:43:13 -0500

    Debugging Attempts

--- fusedObjective.m
+++ fusedObjective.m
@@ -5,20 +5,27 @@ q = size(Theta, 1);
 
 obj = trace((Y - X*B)'*(Y - X*B)) + lambda*sum(sum((B.^2)./slackVar{1}));
 
-pen1 = 0;
-for l = 1:p
-    for m = l:p
-        pen1 = pen1 + Psi(l,m)^2*sum((B(l,:) - sign(Psi(l,m))*B(m,:))./squeeze(slackVar{2}(:, l, m)'));
+if gamma1 ~= 0
+    pen1 = 0;
+    for l = 1:p
+        for m = l:p
+            pen1 = pen1 + Psi(l,m)^2*sum((B(l,:) - sign(Psi(l,m))*B(m,:))./squeeze(slackVar{2}(:, l, m)'));
+        end
     end
 end
-
-pen2 = 0;
-for r = 1:q
-    for s = r:q
-        pen2 = pen2 + Theta(r,s)^2*sum((B(:,r) - sign(Theta(r,s))*B(:,s))./squeeze(slackVar{3}(:, r, s)));
+if gamma2 ~= 0
+    pen2 = 0;
+    for r = 1:q
+        for s = r:q
+            pen2 = pen2 + Theta(r,s)^2*sum((B(:,r) - sign(Theta(r,s))*B(:,s))./squeeze(slackVar{3}(:, r, s)));
+        end
     end
 end
-
-obj = obj + gamma1*pen1 + gamma2*pen2;
+if gamma1 ~= 0
+    obj = obj + gamma1*pen1;
+end
+if gamma2 ~= 0
+    obj = obj + gamma2*pen2;
+end
 
 end
